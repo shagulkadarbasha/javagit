7da935f4c4e6e1baf29c23e19f6eadabc17545a9
shagulkadarbasha <shagulmech@gmail.com>
2022-01-10 17:21:11 +0530

    masterproject

---
+++ src/org/hit/Employee.java
@@ -0,0 +1,15 @@
+package org.hit;
+
+public class Employee {
+	private void sample() {
+		System.out.println("system");
+
+	}
+
+	public static void main(String[] args) {
+	
+Employee e = new Employee();
+e.sample();
+	}
+
+}
