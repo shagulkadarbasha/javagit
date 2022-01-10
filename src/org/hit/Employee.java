4530efe71689a070087f26a41e3c5a4a254874a6
shagulkadarbasha <shagulkmech@gmail.com>
2022-01-10 19:30:51 +0530

    recently add

--- src/org/hit/Employee.java
+++ src/org/hit/Employee.java
@@ -1,21 +1,21 @@
 package org.hit;
 
 public class Employee {
-	public void sample() {
-		System.out.println("system");
+	public void sugar() {
+		System.out.println("driver");
 
 	}
 
 	public static void main(String[] args) {
 	
 Employee e = new Employee();
-e.sample();
-e.mul();
+e.sugar();
+e.nul();
 	}
-	public void mul() {
-		System.out.println("multiplication");
+	public void nul() {
+		System.out.println("merge");
 }
-	public void mull() {
-		System.out.println("multi");	
+	public void sample2() {
+		System.out.println("mugal");	
 }
 }
