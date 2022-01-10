79e5a638a511f97ead3f776f99974d70be6db1fe
shagulkadarbasha <shagulkmech@gmail.com>
2022-01-10 17:58:15 +0530

    manichanges

--- src/org/hit/Employee.java
+++ src/org/hit/Employee.java
@@ -10,9 +10,9 @@ public class Employee {
 	
 Employee e = new Employee();
 e.sample();
-e.add();
+e.sub();
 	}
-	public void add() {
-		System.out.println("addition");
+	public void sub() {
+		System.out.println("subttraction");
 }
 }
